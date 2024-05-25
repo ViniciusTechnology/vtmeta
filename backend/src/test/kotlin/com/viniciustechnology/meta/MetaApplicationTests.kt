ab0cf868c75bf992873a5a0c55458c3fb270d6b6
ViniciusTechnology <vinicius.barreto994@gmail.com>
2024-05-25 20:00:42 -0300

    project created

---
+++ backend/src/test/kotlin/com/viniciustechnology/meta/MetaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.viniciustechnology.meta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MetaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
